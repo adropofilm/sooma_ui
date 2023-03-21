620194ab6354d3e518ec705590fbea5203928b82
Fatima Mohamed <adropofilm@gmail.com>
2023-03-21 00:34:01 -0500

    Styling to shift dropdown menu down when clicked

--- src/components/Layout/Header/BurgerMenu/BurgerMenu.tsx
+++ src/components/Layout/Header/BurgerMenu/BurgerMenu.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, useState } from "react";
 import { useMatch } from "react-router-dom";
 import { PATHS } from "../../../../utils/constants";
+import XIcon from "../../../common/Icons";
 import BurgerMenuIcon from "../BurgerMenuIcon";
 import NavItem from "../NavItem";
 
@@ -15,35 +16,19 @@ export const BurgerMenu = (): ReactElement => {
   };
 
   return (
-    <div className="relative w-full md:hidden">
+    <div className="md:hidden">
       <BurgerMenuIcon
-        classes={`${showDropdown ? "hidden" : ""} z-10 ml-auto`}
+        classes={`${showDropdown ? "hidden" : ""}`}
         action={activateBurgerMenu}
       />
-      <div className={`${showDropdown ? "" : "xs:hidden"} w-full absolute `}>
-        <div className="relative z-20 bg-midnight w-full rounded-lg">
-          <div onClick={() => setShowDropdown(false)}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={3}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-            <NavItem active={!!homePage} label="home" to={PATHS.HOME} />
-            <NavItem
-              active={!!quizzesPage}
-              label="quizzes"
-              to={PATHS.QUIZZES}
-            />
-          </div>
+      <div
+        onClick={() => setShowDropdown(false)}
+        className={`${showDropdown ? "" : "hidden"} relative`}
+      >
+        <XIcon />
+        <div className="w-28 absolute top-9 border-t-8 border-midnight -left-14 bg-midnight rounded-b-lg">
+          <NavItem active={!!homePage} label="home" to={PATHS.HOME} />
+          <NavItem active={!!quizzesPage} label="quizzes" to={PATHS.QUIZZES} />
         </div>
       </div>
     </div>
