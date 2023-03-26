58ef8a30018fd5e7021d15be51efbe43fe42dfcd
Fatima Mohamed <adropofilm@gmail.com>
2023-03-26 06:42:30 -0500

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,20 @@ module.exports = {
       colors: {
         midnight: "#1B1C37",
         periwinkle: "#7579FF",
+        elephant: "#7579FF",
+        purple: "#B224EF",
       },
       screens: {
         xs: "320px",
+      },
+      fontSize: {
+        sm: "0.8rem",
+        base: "1rem",
+        xl: "1.25rem",
+        "2xl": "1.563rem",
+        "3xl": "1.953rem",
+        "4xl": "2.441rem",
+        "5xl": "3.052rem",
       },
     },
   },
